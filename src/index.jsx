85450a2f4d775636067cb4d41d5f03d6e6a4122c
Cyber Slug <lasminski@mail.ru>
2022-03-15 04:51:22 +0300

    feat: configure redux-persist lib

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
-
 import App from "./components/App.jsx"
 
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
-import {store} from './redux/index'
+import store, {persistor} from './redux/index'
+import { PersistGate } from 'redux-persist/integration/react'
 
 render (
     <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
         <App/>
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 )
--- src/redux/index.js
+++ src/redux/index.js
@@ -4,9 +4,21 @@ import todos from "./reducers/todos.reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
+import { persistStore, persistReducer, } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
 const rootReducer = combineReducers( {
     todos,
     filterTodo
 })
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)))
+export const persistor = persistStore(store)
+export default store
+
